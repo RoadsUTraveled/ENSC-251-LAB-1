21ecd3a6c017c71393c339fd0c05a32d1d5a59ec
LanQing Luo <roadsutraveled0729@gmail.com>
2022-09-25 19:45:59 -0700

    Initial Commit

---
+++ ENSC 251 LAB 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ENSC 251 LAB 1
+//
+//  Created by Luo LanQing on 2022/9/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
